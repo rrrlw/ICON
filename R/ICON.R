fabdbe22e2b0cfaf8fde78f41fe1ae1cbc18a078
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-11-29 17:07:53 -0500

    Added package documentation

---
+++ R/ICON.R
@@ -0,0 +1,10 @@
+#' ICON: Provides Easy Access to Complex Systems Datasets
+#'
+#' Provides easy-to-use and easy-to-access datasets from the
+#' Index of COmplex Networks (ICON) database available on the University
+#' of Colorado website. All datasets can be loaded with a single function
+#' call and new datasets are being slowly added from ICON.
+#'
+#' @docType package
+#' @name ICON
+NULL
