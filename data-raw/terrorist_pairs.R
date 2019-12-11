23f2ecd7f8ef60fb810d404160be45ea390442f1
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-11 16:22:34 -0500

    Added terrorist_pairs data and rda

---
+++ data-raw/terrorist_pairs.R
@@ -0,0 +1,6 @@
+# read in data
+terrorist_pairs <- read.table("data-raw/terrorist.pairs")
+colnames(terrorist_pairs) <- c("Node1", "Node2")
+
+# save data frame as rda
+usethis::use_data(terrorist_pairs, overwrite = TRUE)
