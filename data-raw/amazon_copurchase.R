b4cee0d12ad0659705933c972b1f31fae40b813c
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-11 19:47:27 -0500

    Added script converting amazon_copurchase raw to rda

---
+++ data-raw/amazon_copurchase.R
@@ -0,0 +1,10 @@
+# read in data
+amazon_copurchase <- read.table("data-raw/amazon_copurchase.txt",
+                                col.names = c("From", "To"),
+                                colClasses = c("character", "character"),
+                                stringsAsFactors = FALSE,
+                                skip = 4,
+                                nrows = 3387400)
+
+# save as rda
+usethis::use_data(amazon_copurchase, overwrite = TRUE)
