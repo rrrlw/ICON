933aea54c5457b69e4f0b1d82c66f52699f19003
Raoul W <raoulwadhwa@gmail.com>
2020-10-22 14:35:12 -0400

    added unit testing for download

---
+++ tests/testthat/test-download.R
@@ -0,0 +1,16 @@
+test_that("download from host works", {
+  # don't use internet connection on CRAN
+  skip_on_cran()
+  
+  # download dataset (small: < 1 KB)
+  ICON::get_data("aishihik_intensity")
+  
+  # make sure download went fine
+  expect_equal(nrow(aishihik_intensity), 78)
+  expect_equal(ncol(aishihik_intensity), 3)
+  expect_equal(colnames(aishihik_intensity),
+               c("Fish", "Parasite", "Intensity"))
+  expect_equal(class(aishihik_intensity$Fish), "character")
+  expect_equal(class(aishihik_intensity$Parasite), "character")
+  expect_equal(class(aishihik_intensity$Intensity), "numeric")
+})
