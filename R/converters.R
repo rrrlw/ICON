26448d2bd2a007b5443b05457f1910ede5533173
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-11-04 15:30:20 -0500

    use ICON S3 class in as.ICON

--- R/converters.R
+++ R/converters.R
@@ -1,8 +1,7 @@
 # S3 generic method for constructing an ICON object from data.frame
 # INTERNAL USE ONLY - DO NOT EXPORT (WOULD NEED EXTENSIVE MODIFICATIONS)
 as.ICON <- function(df) {
-  class(df) <- c("ICON", "data.frame")
-  return(df)
+  return(ICON(df))
 }
 
 #' Convert ICON objects to network objects
